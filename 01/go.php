9820bff557f113adff79e472bfdf4012b414abd8
Linas Kriščiukaitis <l.krisciukaitis@gmail.com>
2021-05-26 23:52:34 +0300

    #5

--- 01/go.php
+++ 01/go.php
@@ -11,29 +11,84 @@ echo '<br>';
 
 // 2 
 
-$pirmas = rand(0,4);
-$antras = rand(0,4);
+// $pirmas = rand(0,4);
+// $antras = rand(0,4);
 
-echo $pirmas;
+// echo $pirmas;
+// echo '<br>';
+// echo $antras;
+// echo '<br>';
+
+
+// if ($pirmas> $antras) {
+//     if ($antras != 0){
+//         echo round($pirmas/$antras,2);
+//     }else{
+//         echo 'dalyba is 0 negalima';
+//     }
+// } else {
+//     if ($pirmas != 0) {
+//         echo round($antras/$pirmas,2);
+//     } else {
+//         echo 'dalyba is 0 negalima';
+//     }
+// }
+// echo '<br>';
+
+// 3 
+
+$pirmas = rand(0,25);
+$antras = rand(0,25);
+$trecias = rand(0,25);
+
+
+echo $pirmas, ', ', $antras,', ', $trecias;
 echo '<br>';
-echo $antras;
+echo '-----------';
 echo '<br>';
 
-
-if ($pirmas> $antras) {
-    if ($antras != 0){
-        echo round($pirmas/$antras,2);
-    }else{
-        echo 'dalyba is 0 negalima';
+function vidurinis($a,$b,$c){
+                                // reikia fix'o
+    if($a>$b && $a<$c){
+        echo $a;
+    }else if($b>$a && $b<$c){
+        echo $b;
+    }else if($c>$a && $c<$b){
+        echo $c;
     }
-} else {
-    if ($pirmas != 0) {
-        echo round($antras/$pirmas,2);
+    
+}
+vidurinis($pirmas, $antras, $trecias);
+
+echo '<br>';
+echo '==========';
+echo '<br>';
+// 4 
+
+$a = rand(1,10);
+$b = rand(1,10);
+$c = rand(1,10);
+
+echo $a, ', ', $b,', ', $c, '<br>';
+
+function arTrikampis($a, $b, $c){
+    if(($a + $b) > $c && ($a + $c) > $b && ($b + $c) > $a ){
+        echo 'Trikampis gaunasi';
     } else {
-        echo 'dalyba is 0 negalima';
+        echo 'Trikampis nesigauna';
     }
 }
+arTrikampis($a, $b, $c);
 
+echo '<br> ========== <br>';
 
+// 5 
+
+$k1 = rand(0,2);
+$k2 = rand(0,2);
+$k3 = rand(0,2);
+$k4 = rand(0,2);
+
+echo $k1, ', ', $k2,', ', $k3, ', ', $k4,'<br>';
 
 
