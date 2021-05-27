f178f7e25f2370011371471b0780465b72a31ceb
Linas Kriščiukaitis <l.krisciukaitis@gmail.com>
2021-05-27 14:50:44 +0300

    #9 puse done

--- _namu_darbai/kintamieji/go.php
+++ _namu_darbai/kintamieji/go.php
@@ -1,46 +1,50 @@
 <?php
 // 1
+echo '1) <br>';
+
 $vardas = 'Linas';
 $pavarde = 'Kri';
 $gimMetai = 1991;
 $esamiMetai = 2021;
 
 
-echo 'As esu ', $vardas, ' ', $pavarde,' . Man yra ', $esamiMetai - $gimMetai, ' metu. <br>';
-
+echo 'As esu ', $vardas, ' ', $pavarde,' . Man yra ', $esamiMetai - $gimMetai, ' metu.';
+echo '<br> ========== <br>';
 
 // 2 
+echo '2) <br>';
 
-// $pirmas = rand(0,4);
-// $antras = rand(0,4);
+$pirmas = rand(0,4);
+$antras = rand(0,4);
 
-// echo $pirmas, '<br>';
-// echo $antras, '<br>';
+echo $pirmas, '<br>';
+echo $antras, '<br>';
 
 
-// if ($pirmas> $antras) {
-//     if ($antras != 0){
-//         echo round($pirmas/$antras,2);
-//     }else{
-//         echo 'dalyba is 0 negalima';
-//     }
-// } else {
-//     if ($pirmas != 0) {
-//         echo round($antras/$pirmas,2);
-//     } else {
-//         echo 'dalyba is 0 negalima';
-//     }
-// }
-// echo '<br>';
+if ($pirmas> $antras) {
+    if ($antras != 0){
+        echo round($pirmas/$antras,2);
+    }else{
+        echo 'dalyba is 0 negalima';
+    }
+} else {
+    if ($pirmas != 0) {
+        echo round($antras/$pirmas,2);
+    } else {
+        echo 'dalyba is 0 negalima';
+    }
+}
+echo '<br>=========<br>';
 
 // 3 
+echo '3) <br>';
 
-$pirmas = rand(0,25);
-$antras = rand(0,25);
-$trecias = rand(0,25);
+$pirm = rand(0,25);
+$antr = rand(0,25);
+$trec = rand(0,25);
 
 
-echo $pirmas, ', ', $antras,', ', $trecias;
+echo $pirm, ', ', $antr,', ', $trec;
 echo '<br>----------- <br>';
 
 function vidurinis($a,$b,$c){
@@ -54,11 +58,12 @@ function vidurinis($a,$b,$c){
     }
     
 }
-vidurinis($pirmas, $antras, $trecias);
+vidurinis($pirm, $antr, $trec);
 
 echo '<br> ========== <br>';
 
 // 4 
+echo '4) <br>';
 
 $a = rand(1,10);
 $b = rand(1,10);
@@ -78,6 +83,7 @@ arTrikampis($a, $b, $c);
 echo '<br> ========== <br>';
 
 // 5 
+echo '5) <br>';
 
 $k1 = rand(0,2);
 $k2 = rand(0,2);
@@ -153,11 +159,13 @@ kiekSkaitmenu($k1,$k2,$k3,$k4);
 echo '<br> ========== <br>';
 
 // 6 
+echo '6) <br>';
 
 $busimasH = rand(1,6);
 echo '<h', $busimasH, '>', $busimasH, '</h', $busimasH,'>', '<br>';
-//    <h      4        >
+
 // 7
+echo '7) <br>';
 
 $sk1 = rand(-10,10);
 $sk2 = rand(-10,10);
@@ -197,6 +205,7 @@ coloredValues($sk1,$sk2,$sk3);
 echo '<br> ========== <br>';
 
 // 8
+echo '8) <br>';
 
 $zvakiuKiekis = rand(5, 3000);
 
@@ -215,3 +224,16 @@ zvakiuKaina($zvakiuKiekis);
 echo '<br> ========== <br>';
 
 // 9
+echo '9) <br>';
+
+$kint1 = rand(0,100);
+$kint2 = rand(0,100);
+$kint3 = rand(0,100);
+
+echo $kint1, ' ',$kint2, ' ',$kint3,'<br>'; 
+
+function avg($a,$b,$c){
+    echo ($a+$b+$c)/3;
+}
+avg($kint1, $kint2, $kint3);
+
