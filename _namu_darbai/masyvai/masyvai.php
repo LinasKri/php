269f860206be059ed5b2b7d75af88282be868257
Linas Kriščiukaitis <l.krisciukaitis@gmail.com>
2021-06-03 16:00:49 +0300

    general update

--- _namu_darbai/masyvai/masyvai.php
+++ _namu_darbai/masyvai/masyvai.php
@@ -8,9 +8,13 @@ $masyvas = [];
 $tenOrMore = 0;
 $size = 30;
 
-for ($i = 0; $i < $size; $i++) { 
+foreach (range(1,30) as $_) {
     $masyvas[] = rand(5,25);
 }
+
+// for ($i = 0; $i < $size; $i++) { 
+//     $masyvas[] = rand(5,25);
+// }
 
 echo'<pre>';
 print_r($masyvas);
@@ -20,41 +24,64 @@ echo '<br><hr>';
 // 2 ///////////////////////////////////////////////////////////////////////////
 echo '2) <br>';
 echo'a) <br>';//-----------------------------
-foreach ($masyvas as $key => $value) {
+foreach ($masyvas as $value) {
     if ($value >= 10) {
         $tenOrMore++;
     }
 }
 echo "Elementai didesni uz 10: $tenOrMore <br>";
 
+
 echo'b) <br>';//-----------------------------
-$biggest = 0;
+
+$max = 0;
+$indexes = []; 
 foreach ($masyvas as $key => $value) {
 
-    if($value > $biggest){
-        $biggest =  $value;
+    if($value > $max){
+        $max =  $value;
+        $indexes = []; 
+    }
+    if ($value == $max) {
+        $indexes[] = $key; 
     }
 }
-echo "Max reiksme: $biggest <br>";
-echo 'Index: '.array_search($biggest,$masyvas);
+echo $max.'<br>';
+print_r($indexes);
+
+// $biggest = 0;
+// foreach ($masyvas as $key => $value) {
+
+//     if($value > $biggest){
+//         $biggest =  $value;
+//     }
+// }
+// echo "Max reiksme: $biggest <br>";
+// echo 'Index: '.array_search($biggest,$masyvas);
+
 
 echo'<br>c) <br>';//-----------------------------
+
 $sum = 0;
 foreach ($masyvas as $key => $value) {
     if ($key % 2 === 0) {
         $sum += $value;
     }
 }
-echo "Poriniu index'u suna: $sum";
+echo "Poriniu index'u suma: $sum";
+
 
 echo'<br>d) <br>';//-----------------------------
+
 $masyvas_d = [];
 foreach ($masyvas as $key => $value) {
     array_push($masyvas_d,($value-$key));
 }
 print_r($masyvas_d);
 
+
 echo'<br>e) <br>';//-----------------------------
+
 for ($i=0; $i < 10 ; $i++) { 
     array_push($masyvas_d, rand(5,25));
 }
@@ -208,16 +235,18 @@ print_r($masyvas_8);
 
 echo '<br><hr>';
 // 9 ///////////////////////////////////////////////////////////////////////////
-echo '9) <
-br>';
+echo '9) <br>';
+
 $masyvas_9 = [];
 
 foreach ($masyvas6_1 as $key => $value) {
     $masyvas_9[$key] = $value;
 }
+
 foreach ($masyvas6_2 as $key => $value) {
-    $masyvas_9[$value] = $value;
+    array_push($masyvas_9,$value);
 }
+
 print_r($masyvas_9);
 
 echo '<br><hr>';
