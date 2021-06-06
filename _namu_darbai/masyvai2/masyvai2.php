867b8a6ba42032b1af0a00f0e2de106036bfeaf3
Linas Kriščiukaitis <l.krisciukaitis@gmail.com>
2021-06-06 23:14:49 +0300

    commit

--- _namu_darbai/masyvai2/masyvai2.php
+++ _namu_darbai/masyvai2/masyvai2.php
@@ -4,8 +4,10 @@
 // 1 ///////////////////////////////////////////////////////////////////////////
 echo '1) <br>';
 
+$masyvas = [];
+
 foreach (range(1,10) as $keyBig => $_) {
-    foreach (range(1,5) as $keySmall => $_) {
+    foreach (range(1,5) as $keySmall => $smallValue) {
         $masyvas[$keyBig][$keySmall] = rand(5,25);
     }
 }
@@ -18,3 +20,19 @@ echo '<br><hr>';
 // 2 ///////////////////////////////////////////////////////////////////////////
 echo '2) <br>';
 echo'a) <br>';//--------------a---------------
+
+$counter = 0;
+
+foreach ($masyvas as $value) {
+    foreach ($value as $smallValue) {
+        if ($smallValue > 10) {
+            $counter++;
+        }
+    }
+}
+echo "vertes didesnes uz 10: $counter";
+
+
+echo'<br>b) <br>';//--------------b---------------
+
+print_r(max($masyvas));
