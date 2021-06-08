650febc45d2aab81572101f6bfd828f3cd85edbe
Linas Kriščiukaitis <l.krisciukaitis@gmail.com>
2021-06-08 19:00:12 +0300

    visko po biski

---
+++ _namu_darbai/funkcijos/funk.php
@@ -0,0 +1,36 @@
+<body style="background-color:mediumseagreen ;"></body>
+
+<?php
+// 1 ///////////////////////////////////////////////////////////////////////////
+echo '1) <br>';
+
+function insertToH1($string){
+    return "<h1> $string </h1>";
+}
+
+echo insertToH1('labas');
+
+
+// 2 ///////////////////////////////////////////////////////////////////////////
+echo '<br><hr>';echo '2) <br>';
+
+function makeATag($string, $int){
+    return "<h$int> $string </h$int>";
+}
+echo makeATag('Ate', rand(1,6));
+
+
+// 3 ///////////////////////////////////////////////////////////////////////////
+echo '<br><hr>';echo '3) <br>';
+
+$str =  md5(time());
+echo $str.'<br>';
+
+$int = filter_var($str, FILTER_SANITIZE_NUMBER_INT);
+echo "<h1>$int</h1>";
+
+$pattern = '/[0-9]/';
+$result = preg_replace_callback($pattern, 'insertToH1', $int);
+
+
+
--- _namu_darbai/masyvai2/masyvai2.php
+++ _namu_darbai/masyvai2/masyvai2.php
@@ -49,29 +49,29 @@ echo $max;
 
 echo'<br>';echo'<br>c) <br>';//--------------c---------------
 
-foreach ($masyvas as $value) {
-    foreach ($value as $key => $value2) {
-        match ($key) {
-            0 => $sum += $value2,
-            1 => $sum1 += $value2,
-            2 => $sum2 += $value2,
-            3 => $sum3 += $value2,
-            4 => $sum4 += $value2,
-        };
-    }
-}
-_d("$sum $sum1 $sum2 $sum3 $sum4", '2c');
+// foreach ($masyvas as $value) {
+//     foreach ($value as $key => $value2) {
+//         match ($key) {
+//             0 => $sum += $value2,
+//             1 => $sum1 += $value2,
+//             2 => $sum2 += $value2,
+//             3 => $sum3 += $value2,
+//             4 => $sum4 += $value2,
+//         };
+//     }
+// }
+// _d("$sum $sum1 $sum2 $sum3 $sum4", '2c');
 
-// $reiksmes = [];
-// $reiksme = 0;
-// foreach (range(1, 5) as $key => $value) {
-// foreach (range(1, 10) as $key2 => $value2) {
-// $reiksme += $masyvas[$key2][$key];
-// }
-// $reiksmes[] = $reiksme;
-// $reiksme = 0;
-// }
-// print_r($reiksmes);
+$reiksmes = [];
+$reiksme = 0;
+foreach (range(1, 5) as $key => $value) {
+foreach (range(1, 10) as $key2 => $value2) {
+$reiksme += $masyvas[$key2][$key];
+}
+$reiksmes[] = $reiksme;
+$reiksme = 0;
+}
+print_r($reiksmes);
 
 
 
@@ -83,6 +83,7 @@ foreach ($masyvas as $index_ii => &$value) {
     $value[] =  rand(5, 25);
     $value[] =  rand(5, 25);
 }
+print_r($masyvas);
 
 // foreach ($masyvas as $key => $_) {
 //     $masyvas[$key][] = rand(5, 25);
@@ -91,3 +92,103 @@ foreach ($masyvas as $index_ii => &$value) {
 // _d($masyvas, '2d');
 
 echo'<br>';echo'<br>e) <br>';//--------------e---------------
+
+// $sum = 0;
+
+// foreach($masyvas as $x){
+//     foreach($x as $y){
+//         $sum += $y;
+//     }
+//     $naujasMasyvas[] = $sum;
+//     $sum = 0;
+// }
+// _d($naujasMasyvas, 'Naujas masyvas, Antras namu darbas: E');
+
+foreach($masyvas as $i => $val) {
+    $sumos[$i]=array_sum($val);
+}
+
+print_r($sumos);
+
+echo '<br><hr>';
+// 3 ///////////////////////////////////////////////////////////////////////////
+echo '3) <br>';
+for ($ii = 0; $ii < 10; $ii++) {
+    $iSize = rand(2, 20);
+    for ($i = 0; $i < $iSize; $i++) {
+        $masyvas2[$ii][$i] = range('A', 'Z')[rand(0, 25)];
+    }
+    sort($masyvas2[$ii]);
+}
+_d($masyvas2, '5-3');
+
+
+echo '<br><hr>';
+// 4 ///////////////////////////////////////////////////////////////////////////
+echo '4) <br>';
+
+sort($masyvas2);
+
+_d($masyvas2, '5-4');
+
+$indexes_K_sorted = [];
+
+foreach ($masyvas2 as $ii => $value_ii) {
+    if (in_array('K', $value_ii)) {
+
+        $indexes_K_sorted[] = $ii;
+
+    }
+}
+
+foreach ($masyvas2 as $ii => $value_ii) {
+    if (!in_array($ii, $indexes_K_sorted)) {
+
+        $indexes_K_sorted[] = $ii;
+
+    }
+
+}
+
+// _d($indexes_K_sorted);
+
+foreach ($indexes_K_sorted as $index => $value) {
+    $masyvas2_KSorted[$index] = $masyvas2[$value];
+}
+$masyvas2 = $masyvas2_KSorted;
+
+_d($masyvas2, '5-4');
+
+echo '<br><hr>';
+// 5 ///////////////////////////////////////////////////////////////////////////
+echo '5) <br>';
+$masyvas = [];
+
+$count = 0;
+
+do {
+    $count++;
+    $uid = rand(1, 50);
+    foreach ($masyvas as $user) {
+        if ($user['user_id'] == $uid) {
+            continue 2;
+        }
+    }
+    $masyvas[] = [
+        'user_id' => $uid,
+        'place_in_row' => rand(0, 100)
+    ];
+} while(count($masyvas) < 30);
+
+
+echo '<br><hr>';
+// 6 ///////////////////////////////////////////////////////////////////////////
+echo '6) <br>';
+
+echo '<br><hr>';
+// 7 ///////////////////////////////////////////////////////////////////////////
+echo '7) <br>';
+
+echo '<br><hr>';
+// 8 ///////////////////////////////////////////////////////////////////////////
+echo '8) <br>';
