bd5ab61b0a69c402aa5bf55c406b609874566bcf
Linas Kriščiukaitis <l.krisciukaitis@gmail.com>
2021-06-11 12:22:24 +0300

    #1 update

--- _namu_darbai/web-mechanika/01/index.php
+++ _namu_darbai/web-mechanika/01/index.php
@@ -1,3 +1,14 @@
+<?php
+_d($_GET);
+
+$color = 'black';
+if (isset($_GET['color'])) {
+    $color = 'red';
+}
+
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -5,30 +16,10 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>01</title>
-    <style>
-    body{
-        background-color: black;
-    }
-    a{
-        color: white;
-    }
-    </style>
 </head>
-<body>
-    <a href="./"><h2>WEB 01</h2></a>
-    <a href="?color=1"><h2>WEB 01 +</h2></a>
+<body style="background-color: <?=$color?>;">
+    <a href="./index.php"><h2>Black</h2></a>
+    <a href="./index.php?color=1"><h2>Red</h2></a>
 </body>
 </html>
 
-<?php
-if ($_GET['color'] == '1')
-{
-    echo '<style>
-    body{
-        background-color: red;
-    }
-    a{
-        color: black;
-    }
-    </style>';
-}
