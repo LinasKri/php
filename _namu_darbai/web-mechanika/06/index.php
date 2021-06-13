fedfedb36727ae7f6c8b2c551c8411e64a65e68a
Linas Kriščiukaitis <l.krisciukaitis@gmail.com>
2021-06-13 21:02:46 +0300

    #6 done

--- _namu_darbai/web-mechanika/06/index.php
+++ _namu_darbai/web-mechanika/06/index.php
@@ -1,9 +1,13 @@
+<!-- Padarykite puslapį su dviem mygtukais. Mygtukus įdėkite į dvi skairtingas formas- vieną GET ir kitą POST. Nenaudodami jokių konkrečių $_GET ar $_POST reikšmių, nuspalvinkite foną žaliai, kai paspaustas mygtukas iš GET formos ir geltonai- kai iš POST. -->
 
 <?php
+
 _d($_SERVER['REQUEST_METHOD']);
 
-if (condition) {
-    # code...
+if ($_SERVER['REQUEST_METHOD'] == 'GET') {
+    $color = 'green';
+}elseif ($_SERVER['REQUEST_METHOD'] == 'POST'){
+    $color = 'yellow';
 }
 
 ?>
@@ -14,17 +18,17 @@ if (condition) {
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>06</title>
 </head>
 
 <body style="background-color: <?=$color?>;">
 
     <form action="" method="get">
-        <button type="submit">GET</button>
+        <button type="submit">GET </button>
     </form>
 
     <form action="" method="post">
-        <button type="submit">Post</button>
+        <button type="submit">POST</button>
     </form>
 
 </body>
