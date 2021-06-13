e9bf3657a038b5b703bed90dc54a8e44d21efbc0
Linas Kriščiukaitis <l.krisciukaitis@gmail.com>
2021-06-13 19:34:15 +0300

    #3

--- _namu_darbai/web-mechanika/03/index.php
+++ _namu_darbai/web-mechanika/03/index.php
@@ -1,5 +1,15 @@
 <!-- Perdarykite 2 uždavinį taip, kad spalvą būtų galimą įrašyti į laukelį ir ją išsiųsti mygtuku GET metodu formoje. -->
+<?php
 
+_d($_SERVER['REQUEST_METHOD']);
+_d($_GET);
+
+if (isset($_GET['color'])) {
+    $color = $_GET['color'];
+    echo '<body style="background-color: '.($_GET['color']).';"></body>';
+}
+
+?>
 
 
 <!DOCTYPE html>
@@ -8,11 +18,11 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>03</title>
 </head>
 <body>
     <form action="" method="get">
-    <input type="text">
+    <input type="text" name="color">
     <button type="submit">Accept</button>
     </form>
 </body>
