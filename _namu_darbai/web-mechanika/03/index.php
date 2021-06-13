377a8b249c0682073831872248f68f6a5ff8805c
Linas Kriščiukaitis <l.krisciukaitis@gmail.com>
2021-06-13 18:53:54 +0300

    commit

---
+++ _namu_darbai/web-mechanika/03/index.php
@@ -0,0 +1,19 @@
+<!-- Perdarykite 2 uždavinį taip, kad spalvą būtų galimą įrašyti į laukelį ir ją išsiųsti mygtuku GET metodu formoje. -->
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="" method="get">
+    <input type="text">
+    <button type="submit">Accept</button>
+    </form>
+</body>
+</html>
---
+++ _namu_darbai/web-mechanika/06/index.php
@@ -0,0 +1,31 @@
+
+<?php
+_d($_SERVER['REQUEST_METHOD']);
+
+if (condition) {
+    # code...
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body style="background-color: <?=$color?>;">
+
+    <form action="" method="get">
+        <button type="submit">GET</button>
+    </form>
+
+    <form action="" method="post">
+        <button type="submit">Post</button>
+    </form>
+
+</body>
+</html>
