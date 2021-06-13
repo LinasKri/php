00f09d9353eb73a3ffee902f34c65c893c12d183
Linas Kriščiukaitis <l.krisciukaitis@gmail.com>
2021-06-13 23:49:33 +0300

    commit

---
+++ _namu_darbai/web-mechanika/08/rose.php
@@ -0,0 +1,23 @@
+<?php
+// if(header('Location: ./rose.php') == 'Location: ./rose.php'){
+//     header('Location: ./pink.php');
+//     die;
+// }
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Rose</title>
+</head>
+<body style="background-color:red;">
+<form action="pink.php" method="post">
+    <button>GO TO PINK</button>
+    </form>
+</body>
+</html>
