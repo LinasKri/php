953c0f854ba23e70c13ce493965701c0607361b0
Linas Kriščiukaitis <l.krisciukaitis@gmail.com>
2021-06-16 00:24:42 +0300

    apsauga nuo minusin verciu

--- _namu_darbai/bankas/doRemoveFunds.php
+++ _namu_darbai/bankas/doRemoveFunds.php
@@ -1,9 +1,19 @@
 <?php
+function DelayCommand($functionToDelay, $delayTimeInSeconds) {
+    sleep($delayTimeInSeconds);
+    $functionToDelay();
+}
 $id = $_GET['id'] ?? 0;
 foreach ($accounts as &$account) {
     if ($account['id'] == $id) {
-        $account['funds'] -= (int) $_POST['funds'];
-        file_put_contents(__DIR__.'/accounts.json', json_encode($accounts));
-        redirect();
+        if ($account['funds'] > 0) {
+
+            $account['funds'] -= (int) $_POST['funds'];
+            file_put_contents(__DIR__.'/accounts.json', json_encode($accounts));
+            redirect();
+        }else{
+            echo 'insufficient funds!!! Please go back :)';
+            
+        }
     }
 }
