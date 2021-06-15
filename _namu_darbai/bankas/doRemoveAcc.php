54555648b84424fa4b2e3831176842e883c5783e
Linas Kriščiukaitis <l.krisciukaitis@gmail.com>
2021-06-16 00:29:26 +0300

    #2 done

--- _namu_darbai/bankas/doRemoveAcc.php
+++ _namu_darbai/bankas/doRemoveAcc.php
@@ -2,8 +2,12 @@
 $id = $_GET['id'] ?? 0;
 foreach ($accounts as $index => $account) {
     if ($account['id'] == $id) {
-        unset($accounts[$index]);
-        file_put_contents(__DIR__.'/accounts.json', json_encode($accounts));
-        redirect();
+        if ($account['funds'] == 0) {
+            unset($accounts[$index]);
+            file_put_contents(__DIR__.'/accounts.json', json_encode($accounts));
+            redirect();
+        } else {
+            echo 'Account can not be deleted due to current balamce: ' .$account['funds'];
+        }
     }
 }
