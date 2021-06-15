97d9afe8a8a73ed27850dbbd6b0965ce97751c68
Linas Kriščiukaitis <l.krisciukaitis@gmail.com>
2021-06-16 00:36:37 +0300

    #4, #5 done

--- _namu_darbai/bankas/doAddAcc.php
+++ _namu_darbai/bankas/doAddAcc.php
@@ -1,6 +1,10 @@
 <?php
 
 $account = ['name' => $_POST['name'], 'surname' => $_POST['surname'], 'IDCode' => $_POST['IDCode'],'id' => 'LT '.rand(10000000, 999999999), 'funds' => 0];
-$accounts [] = $account;
-file_put_contents(__DIR__.'/accounts.json', json_encode($accounts));
-redirect();
+if (!$account['IDCode'] == 'IDcode') {
+    $accounts [] = $account;
+    file_put_contents(__DIR__.'/accounts.json', json_encode($accounts));
+    redirect();
+}else{
+    echo 'National Id Number has to be unique. Please try again';
+}
