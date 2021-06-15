584729a4fe4fad4f684638a23938711797e75241
Linas Kriščiukaitis <l.krisciukaitis@gmail.com>
2021-06-15 19:03:19 +0300

    Banko failai ir planas

---
+++ _namu_darbai/bankas/index.php
@@ -0,0 +1,21 @@
+<?php
+session_start();
+
+// Saskaitu saraso atvaizdavimas GET
+
+// Pinigu pridejimo atvaizdavimas GET
+
+// Pinigu pridejimo vykdymas POST
+
+// Pinigu atemimo atvaizdavimas GET
+
+// Pinigu atemimo vykdymas POST
+
+// Naujos saskaitos pridejimo atvaizdavimas GET
+
+// Naujos saskaitos pridejimo vykdymas POST
+
+// Saskaitos istrynimo vykdymas POST
+
+
+
