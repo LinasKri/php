bd71071b733f29d2c6ac96a6fd5920c598f545a9
Linas Kriščiukaitis <l.krisciukaitis@gmail.com>
2021-06-15 19:21:56 +0300

    add funds and acc file update

--- _namu_darbai/bankas/index.php
+++ _namu_darbai/bankas/index.php
@@ -1,14 +1,20 @@
 <?php
 session_start();
 
+// acc ['id' => x, 'funds' => n ]
+
 // jei nera .json failo, kuris laikytu saskaitas- sukurk
 if (!file_exists(__DIR__.'/accounts.json')) {
     file_put_contents(__DIR__.'/accounts.json', json_encode([]));
 }
+$accounts = json_decode(file_get_contents(__DIR__.'/accounts.json'), 1) ;
 
 // Saskaitu saraso atvaizdavimas GET
 
 // Pinigu pridejimo atvaizdavimas GET
+if($_GET['action'] == 'add' && $_SERVER['REQUEST_METHOD'] == 'GET'){
+    require __DIR__.'/add.php';
+}
 
 // Pinigu pridejimo vykdymas POST
 
@@ -17,6 +23,9 @@ if (!file_exists(__DIR__.'/accounts.json')) {
 // Pinigu atemimo vykdymas POST
 
 // Naujos saskaitos pridejimo atvaizdavimas GET
+if($_GET['action'] == 'add-acc' && $_SERVER['REQUEST_METHOD'] == 'GET'){
+    require __DIR__.'/addAcc.php';
+}
 
 // Naujos saskaitos pridejimo vykdymas POST
 
