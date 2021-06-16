ef41cf2c6e9a9f806d15f3598cb30e28d18f09ba
Linas Kriščiukaitis <l.krisciukaitis@gmail.com>
2021-06-17 01:35:51 +0300

    login/members

---
+++ klases_darbai/13/menu.php
@@ -0,0 +1,5 @@
+<div>
+    <a href="http://localhost/_php_/klases_darbai/13/">Home</a>
+    <a href="http://localhost/_php_/klases_darbai/13/member.php">Member</a>
+    <a href="http://localhost/_php_/klases_darbai/13/login.php">Login</a>
+</div>
