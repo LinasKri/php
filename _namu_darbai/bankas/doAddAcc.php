578c7d6c58ab4d70209a81502d089017666a1bef
Linas Kriščiukaitis <l.krisciukaitis@gmail.com>
2021-06-21 02:09:53 +0300

    #10

--- _namu_darbai/bankas/doAddAcc.php
+++ _namu_darbai/bankas/doAddAcc.php
@@ -1,23 +1,30 @@
 <?php
 
 $account = ['name' => $_POST['name'],
-        	'surname' => $_POST['surname'], 
+			'surname' => $_POST['surname'], 
 			'IDCode' => $_POST['IDCode'],
 			'id' => $_POST['id'], 
 			'funds' => 0];
 
 if (!$_POST['name']== null && !$_POST['surname'] == null && !$_POST['IDCode'] == null) {
+
 	if(strlen($_POST['name']) >= 3 && strlen($_POST['surname']) >= 3){
-		
-        $accounts [] = $account;
-        file_put_contents(__DIR__.'/accounts.json', json_encode($accounts));
-        setMessage('Nauja saskaita paruosta');
-        redirect();
-	}else{
+
+		if(strlen($_POST['IDCode']) == 11){
+
+			$accounts [] = $account;
+			file_put_contents(__DIR__.'/accounts.json', json_encode($accounts));
+			setMessage('Nauja saskaita paruosta');
+			redirect();
+		} else {
+			setMessage('Neteisingas AK');
+			redirectToAction('add-acc');
+		}
+	} else {
 		setMessage('Vardas ir pavarde negali buti trumpesni kaip 3 simboliai');
         redirectToAction('add-acc');
 	}
-} else{
+} else {
         setMessage('All fields has to be filled');
         redirectToAction('add-acc');
 }
