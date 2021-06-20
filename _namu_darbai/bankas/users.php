24961ceb07f13f55868026714990be0d4aba16c9
Linas Kriščiukaitis <l.krisciukaitis@gmail.com>
2021-06-21 02:23:15 +0300

    commit

--- _namu_darbai/bankas/users.php
+++
@@ -1,8 +0,0 @@
-<?php 
-
-$users = [
-    ['name' => 'Linas', 'pass' => md5('321')],
-    ['name' => 'Jolanta', 'pass' => md5('321')],
-    ['name' => 'Rokas', 'pass' => md5('321')],
-];
-file_put_contents(__DIR__.'/users.json',json_encode($users));
