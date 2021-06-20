4fd0e26cd61115ac7ccb85979a0b399a9b1501ea
Linas Kriščiukaitis <l.krisciukaitis@gmail.com>
2021-06-21 02:32:03 +0300

    update

--- _namu_darbai/bankas/menu.php
+++ _namu_darbai/bankas/menu.php
@@ -1,5 +1,5 @@
 <div>
-<a href="https://localhost/_php_/_namu_darbai/bankas/users.php">users</a>
+<!-- <a href="https://localhost/_php_/_namu_darbai/bankas/users.php">users</a> -->
 <a href="?action=add-acc">Add account</a>
 <a href="http://localhost/_php_/_namu_darbai/bankas/">Home</a>
 </div>
