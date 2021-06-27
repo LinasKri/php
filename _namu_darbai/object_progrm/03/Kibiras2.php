9681ff2f773a686f47a2e8598f2ff699741f72e4
Linas Kriščiukaitis <l.krisciukaitis@gmail.com>
2021-06-27 12:06:36 +0300

    Kibirai

---
+++ _namu_darbai/object_progrm/03/Kibiras2.php
@@ -0,0 +1,28 @@
+<?php
+
+
+class Kibiras2{
+    protected $akmenuKiekis = 0;
+
+    static private $akmenuKiekisVisuoseKibiruose = 0;
+
+
+    public function prideti1Akmeni(){
+        $this -> akmenuKiekis++;
+    }
+
+    public function pridetiDaugAkmenu($kiekis){
+        
+        if(!is_integer($kiekis)){
+            return;
+        }
+        $this -> akmenuKiekis += $kiekis;
+
+    }
+
+    public function kiekPririnktaAkmenu(){
+        return $this->akmenuKiekis;
+    }
+
+
+}
