5a6e4e046a5cb26d8d1299e9ee95be8cc75da755
Linas Kriščiukaitis <l.krisciukaitis@gmail.com>
2021-07-04 23:59:02 +0300

    commit

--- _namu_darbai/obj_bankas/index.php
+++ _namu_darbai/obj_bankas/index.php
@@ -1,2 +1,24 @@
 <?php
+define('ENTER', true);
+require __DIR__.'/bootstrap.php';
 
+// controller
+require DIR.'app/FundController.php';
+
+
+$uri = str_replace(INSTALL_DIR, '', $_SERVER['REQUEST_URI']);
+$uri = explode('/', $uri);
+
+_d($uri);
+
+// ROUTER
+
+if ($uri[0]== 'testas' && isset($uri[1])) {
+    (new FundController) -> fundTest($uri[1]);
+}
+
+
+else {
+    http_response_code(404);
+    echo '<h1>404 PAGE NOT FOUND!🤷‍♂️';
+}
