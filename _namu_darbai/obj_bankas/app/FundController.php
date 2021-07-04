b0b06f1c9ba7bd5e461fef9659a23c512832d769
Linas Kriščiukaitis <l.krisciukaitis@gmail.com>
2021-07-05 00:32:41 +0300

    views

--- _namu_darbai/obj_bankas/app/FundController.php
+++ _namu_darbai/obj_bankas/app/FundController.php
@@ -4,9 +4,11 @@ class FundController {
 
 
     public function fundTest($say = '') {
-
         echo 'FundController<br>';
         echo $say;
-
     }
+
+    public function index() {
+        require DIR.'views/home.php';
+    }    
 }
--- _namu_darbai/obj_bankas/index.php
+++ _namu_darbai/obj_bankas/index.php
@@ -9,12 +9,14 @@ require DIR.'app/FundController.php';
 $uri = str_replace(INSTALL_DIR, '', $_SERVER['REQUEST_URI']);
 $uri = explode('/', $uri);
 
-_d($uri);
+// _d($uri);
 
 // ROUTER
 
 if ($uri[0]== 'testas' && isset($uri[1])) {
     (new FundController) -> fundTest($uri[1]);
+}elseif ($uri[0] === '' && count($uri) === 1){
+    (new FundController) -> index();
 }
 
 
