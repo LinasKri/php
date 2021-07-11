dbe0bb1ceeb1d6936b16b4b945d397e16f51230c
Linas Kriščiukaitis <l.krisciukaitis@gmail.com>
2021-07-11 20:24:09 +0300

    update

--- _namu_darbai/bankai/obj_bankas/views/remove.php
+++ _namu_darbai/bankai/obj_bankas/views/remove.php
@@ -1,8 +1,10 @@
 <?php require DIR.'views/top.php' ?>
 
-<form action="<?= URL ?>rem/<?= $id ?>" method="post">
-    <input type="text" name="amount" value="">
-    <button>Remove funds</button>
-</form>
-
+<div style="background-color: coral; padding: 20px">
+    Account No.:<?= $id ?>
+    <form action="<?= URL ?>rem/<?= $id ?>" method="post">
+        <input type="text" name="amount" value="">
+        <button>Remove funds</button>
+    </form>
+</div>
 <?php require DIR.'views/bottom.php' ?>
